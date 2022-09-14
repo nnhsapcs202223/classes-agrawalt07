ba2caf698883381ef2e06eb395917b738afa65dd
Tejas <tagrawal1@stu.naperville203.org>
2022-09-14 14:21:36 -0500

    Day 2

--- MileageTracker.java
+++ MileageTracker.java
@@ -8,7 +8,25 @@ public class MileageTracker
 {
     /*
      * 2. Define the instance variables (store the object's attributes):
+     * specify the visibility (e.g. private)
+     *      public visibility means it is accessible by any code in any class
+     *      private: only accessible by methods in this class
+     *  specify the type (e.g. int, double, String)
+     *  specify the name(e.g. distanceDriven)
+     *  
+     * Instance variables differ from local variables in the following ways:
+     *      scoped to the class (accessible to all methods of the class;
+     *          lifetime is the same as the object)
+     *      automatically initialised to a default value (0, false, null)
+     *      best practice is to not immediately initialis instance variables
+     *      
      */
+     private int distanceDriven; //in units of miles
+     private int fuelConsumed; //in units of gallons
+     private String vin; //vehicle identification
+     
+
+    
     
     
     
@@ -17,6 +35,12 @@ public class MileageTracker
     
     /*
      * 3. Define the constructor(s):
+     *      a contructor is responsible for initialising newly created objects 
+     *      contructors are invoked automatically via the new operator 
+     *      has no return type (not even void)
+     *      name of the constructor must match the name of the class
+     *      multiple constructors may be defined for a class
+     *      one contructor may call another constructor (with restrictions)
      */
     
     
@@ -34,6 +58,28 @@ public class MileageTracker
      * the parameters and their types (e.g., miles or type int) 
      * 
      */
+    /**
+     * Default constructor for the MileageTracker class. 
+     *      Initialises the objects miles driven and feul consumed to 0 
+     *          and the VIN to null.
+     */
+    public MileageTracker()
+    {
+        
+    }
+    
+    /**
+     * Contructs a new Mileage tracker object with the specified 
+     *      miles driven and feul consumed.
+     * @param initialDistanceDriven the number of miles already driven 
+     * @param initalFeulConsumed the number of gallons of feul already consumed 
+     */
+    public MileageTracker(int initialDistanceDriven,int initalFeulConsumed)
+    {
+        
+    }
+    
+    
     /**
      * Increments the number of miles that the car has driven.
      * 
