6e93d5152af70b53b6e7b8b64eafc3b365fcb550
Tejas <tagrawal1@stu.naperville203.org>
2022-09-27 14:20:07 -0500

    l

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,20 +1,53 @@
 import java.util.*;  
 class VendingMachine   
 {  
-private int;     
+private int tokens;
+private ArrayList<String> cans = new ArrayList<String>();
     
+public VendingMachine(int initialTokenAmount, ArrayList<String> initialCans){
+    this.tokens = initialTokenAmount;
+    this.cans = initialCans;
+}
     
-    
-public static void main(String[] args)  
-{  
-Scanner sc= new Scanner(System.in);    //System.in is a standard input stream  
-System.out.print("Enter first number- ");  
-int a= sc.nextInt();  
-System.out.print("Enter second number- ");  
-int b= sc.nextInt();  
-System.out.print("Enter third number- ");  
-int c= sc.nextInt();  
-int d=a+b+c;  
-System.out.println("Total= " +d);  
-}  
+
+public VendingMachine(){
+    this.tokens = 0;
+    this.cans = fillUp();
+}
+
+public int getCanCount(){
+    int amountOfCans = this.cans.size();
+    return amountOfCans;
+}
+
+public int getTokenCount(){
+    return this.tokens;
+}
+
+public void insertToken(){
+    if (getCanCount() > 0){
+        Scanner sc= new Scanner(System.in);    //System.in is a standard input stream  
+        System.out.println("Which soda would you like ");  
+        System.out.println(this.cans);
+        String soda = sc.nextLine();
+        this.tokens += 1;
+        this.cans.remove(soda.toLowerCase());
+    }
+    else{
+        fillUp();
+        insertToken();
+    }
+}
+
+public ArrayList fillUp(){
+    cans.add("pepsi");
+    cans.add("coke");
+    cans.add("deit pepsi");
+    cans.add("deit coke");
+    return cans;
+}
+public static void main(String[] args){
+    VendingMachine vendingMachine = new VendingMachine();
+    vendingMachine.insertToken();
+}
 }  
