e8cca4ef686ead10f6ea73d5fe0ad565046c7b15
Tejas <tagrawal1@stu.naperville203.org>
2022-09-30 14:12:35 -0500

    L

---
+++ TargetViewer.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class Target here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+import javax.swing.JFrame;
+
+public class TargetViewer
+{
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame();  // create frame object
+        // set frame attributes
+        frame.setSize(300, 400);
+        frame.setTitle("Targets");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // initialize a TargetComponent() component object
+        TargetComponent component = new TargetComponent();
+        // add the component to the frame
+        frame.add(component);
+
+        frame.setVisible(true);
+    }
+
+}
