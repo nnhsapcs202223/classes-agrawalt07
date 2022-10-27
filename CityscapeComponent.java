c9acfad4b0dfde50413bfbdf4eb411d71528b925
Tejas <tagrawal1@stu.naperville203.org>
2022-10-26 22:27:51 -0500

    Completed the collision check, documented the code, implemented good incapsulation, added a point feature and the ablilty to see your points

--- CityscapeComponent.java
+++ CityscapeComponent.java
@@ -1,65 +1,114 @@
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import javax.swing.JComponent;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import javax.swing.JFrame; 
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 /**
  * Class that creates instances of the classes that comprise the cityscape and delegates drawing the
  *  cityscape to these object.
- * 
- * @author gcschmit
- * @version 18 July 2014
+ *
+ * @author Tejas Agrawal
+ * @version 23 October 2022
  */
-public class CityscapeComponent extends JComponent implements KeyListener
+public class CityscapeComponent extends JComponent
 {
     // define the objects in your Cityscape as instance variables
     // ...
-    Rect rectangle = new Rect();
-
+    Image image;
+    Graphics graphics;
+    Rect bar;
+    Rect greenPoint;
+    boolean hit;
+    boolean pointGained;
+    int points;
 
     // define the CityscapeComponent contructor and intiailize all instance variables
     // ...
     public CityscapeComponent(){
-        this.addKeyListener(this);
+        bar = new Rect(500, 500, 20, 100, Color.blue);
+        greenPoint = new Rect(50,20,Color.green);
+        hit = false;
+        this.addKeyListener(new AL());
+        this.setFocusable(true);
+        //this.addKeyListener(this);
+
     }
 
     /**
      * This method is invoked by the Java Run-Time whenever the component needs to be redrawn.
      * It does not need to be invoked explicitly.
-     * 
+     *
      * @param g a reference to the Graphics object used for all drawing operations
      *
      */
-    @Override
     public void paintComponent(Graphics g)
     {
-        Graphics2D g2 = (Graphics2D) g;
-        this.rectangle.Draw(g2); 
+        image = createImage(this.getWidth(), this.getHeight());
+        graphics = image.getGraphics();
+        g.drawImage(image, 0, 0, this);
+
+        bar.draw(g);
+        greenPoint.draw(g);
+        greenPoint.moveDown(greenPoint); 
+         g.setColor(Color.red);
+            g.setFont(new Font("MV Boli", Font.PLAIN,25));
+            g.drawString("Points: "+this.points, 25, 40);        
+        if(pointGained){
+            g.setColor(Color.red);
+            g.setFont(new Font("MV Boli", Font.PLAIN,25));
+            g.drawString("Points: "+this.points, 25, 40);
+            this.pointGained=false;
+        }
         // invoke the draw method on each object in your Cityscape
         // ...
 
     }
 
-    @Override
-    public void keyTyped(KeyEvent e){
-
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e){
-        switch(e.getKeyCode()){
-            case 37: rectangle.moveLeft();
-                break;
-        } 
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e){
-
-    }
     
-    
+    /**
+     * A method that checks if 2 Rect objects intersect or touch each other
+     * 
+     * @param pointOrHit The Rect class that you want to compare with player(bar)
+     */
+    public void checkCollision(Rect pointOrHit){
+            if (pointOrHit.getXPos()>=bar.getXPos()) {{
+                    if ((pointOrHit.getXPos()+pointOrHit.getWidthSize())>=(bar.getXPos()) && (pointOrHit.getYPos()+pointOrHit.getHeightSize())>=(bar.getYPos()) && pointOrHit.getGone()==false)
+                    {  
+                        pointOrHit.setGone(true); 
+                        pointOrHit.setColor(Color.blue);
+                        pointOrHit.setWidthSize(0);
+                        pointOrHit.setHeightSize(0);
+                        this.points+=1;
+                        System.out.println(points); 
+                        this.pointGained = true; 
+                    }            
+                }
+            }
+            else if((pointOrHit.getXPos())<=bar.getXPos()+bar.getWidthSize() && (pointOrHit.getYPos())>=bar.getYPos()+bar.getHeightSize() && pointOrHit.getGone()==false){
+                pointOrHit.setGone(true); 
+                pointOrHit.setColor(Color.blue);
+                pointOrHit.setWidthSize(0);
+                pointOrHit.setHeightSize(0);  
+                this.points+=1;
+                this.pointGained=true;
+                System.out.println(points);
+            }
+        }
+
+    public class AL extends KeyAdapter{
+        @Override
+        /**
+         * A method that gets what key is pressed and checks if there is a collision when you pressed a key. 
+         * 
+         * @param e gets what key is pressed
+         */
+        public void keyPressed(KeyEvent e){
+            if (hit!=true){
+                bar.keyPressed(e);
+                checkCollision(greenPoint); 
+                repaint();
+            }
+        }
+    }
+
     /**
      * Animate the cityscape by updating the objects such that they appear to be animated when
      *      they are next drawn.
@@ -67,9 +116,9 @@ public class CityscapeComponent extends JComponent implements KeyListener
      */
     public void nextFrame()
     {
+
         // update the objects in the cityscape so they are animated
-        // ...
-        
+        checkCollision(greenPoint); 
         // request that the Java Runtime repaints this component by invoking its paintComponent method
         //  do not explicitly invoke the paintComponent method
         repaint();
--- Rect.java
+++ Rect.java
@@ -1,46 +1,185 @@
-import java.awt.Color;
+import java.awt.*;
 import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.event.*;
+import java.util.Random;
+/**
+ * This class creates a rectangle with ability to configure the xPos, yPos, height, width, and color. 
+ *
+ * @author Tejas Agrawal
+ * @version 26 Oct. 2022
+ */
+public class Rect extends Rectangle
+{
+    private Color color;
+    private int x;
+    private int y;
+    private int height;
+    private int width;
+    private boolean gone;
+    Rect(int initialX, int initialY, int initialHeight, int initialWidth, Color initialColor){
+        this.x = initialX;
+        this.y = initialY;
+        this.height = initialHeight;
+        this.width = initialWidth;
+        this.color = initialColor;
+        this.gone = false;
+    }
 
-import java.awt.geom.Rectangle2D;
-import java.awt.Graphics2D;
+    Rect(int initialHeight, int initialWidth, Color initialColor)
+{
+    Random number = new Random();
+    this.x = number.nextInt(600);
+    this.y = 0;
+    this.height = initialHeight;
+    this.width = initialWidth;
+    this.color = initialColor;
+}
+    /**
+     * Uses the keyEvent or the key that is presssed that move the Rect object left or right.
+     * 
+     * @param e evaluates what key is pressed
+     */
+
+    public void keyPressed(KeyEvent e){
+        if (e.getKeyCode() == KeyEvent.VK_LEFT & this.x<800 & this.x>0){
+            this.x = x-10;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT & this.x<600 & this.x>0){
+            this.x = x+10;
+        }
+    }
+    
+    /**
+     * returns the integer value of the xPos of the Rect object
+     * 
+     * @return The x position of the Rect object
+     */
+    public int getXPos(){
+        return this.x; 
+    }
+
+    /**
+     * returns the integer value of the width size of the Rect object
+     * 
+     * @return The width size of the Rect object
+     */
+    public int getWidthSize(){
+        return this.width;
+    }
+
+    /**
+     * returns the integer value of the heigth size of the Rect object
+     * 
+     * @return The height size of the Rect object
+     */
+    public int getHeightSize(){
+        return this.height;
+    }
+
+    /**
+     * returns the integer value of the yPos of the Rect object
+     * 
+     * @return The y position of the Rect object
+     */
+        public int getYPos(){
+        return this.y; 
+    }
 
 /**
- * Write a description of class Rect here.
- *
- * @author (your name)
- * @version (a version number or a date)
+ * returns whether the Rect object is gone from the screen or not
+ * 
+ * @return The boolean state of the gone variable of the Rect object
  */
-public class Rect
-{
-    // instance variables - replace the example below with your own
-    private int xPos;
-    private int yPos;
-    
-    /**
-     * Constructor for objects of class Rect
-     */
-    public Rect()
-    {
-        // initialise instance variables
-        this.xPos = 740;
-        this.yPos = 590;
+    public boolean getGone(){
+        return this.gone; 
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * sets the integer value of the xPos of the Rect object
+     * 
+     * @param newX the new x position of the Rect object
      */
-
-    public void Draw(Graphics2D g2)
-    {
-        Rectangle2D.Double body = new Rectangle2D.Double(this.xPos, this.yPos, 60, 10);       
-        g2.setColor(Color.red);
-        g2.fill(body); 
+        public void setXPos(int newX){
+         this.x = newX; 
     }
-    
-    public void moveLeft(){
-        this.yPos+=3;
+
+    /**
+     * sets the integer value of the width size of the Rect object
+     * 
+     * @param newWidth the new width size of the Rect object
+     */
+    public void setWidthSize(int newWidth){
+         this.width = newWidth;
+    }
+
+    /**
+     * sets the integer value of the height of the Rect object
+     * 
+     * @param newHeight the new height size of the Rect object
+     */
+    public void setHeightSize(int newHeight){
+         this.height = newHeight;
+        }
+
+    /**
+     * sets the integer value of the yPos of the Rect object
+     * 
+     * @param newY the new y position of the Rect object
+     */
+        public void setYPos(int newYPos){
+         this.y = newYPos; 
+    }
+
+    /**
+     * sets the new Colour of the Rect object
+     * 
+     * @param newColor the new Colour of the Rect object
+     */
+    public void setColor(Color newColor){
+        this.color = newColor;
+    }
+
+    /**
+     * sets the boolean value of the gone variable of the Rect object
+     * 
+     * @param newGone the new gone state of the Rect object
+     */
+    public void setGone(boolean newGone){
+         this.gone = newGone; 
+    }
+
+    /**
+     * Draws a rectangle from the Rect class with the color, x, y, width, height that is given 
+     * 
+     * @param g The graphics object that is used to draw the rectangle
+     */
+    public void draw(Graphics g){
+        g.setColor(this.color);
+        g.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    /**
+     * Allows a Rect object to fall from the top of the screen to the bottom 
+     * 
+     * @param rectangle The instance of the Rect class that you want to move down the screen
+     */
+    public void moveDown(Rect rectangle){
+
+        try {
+            if (rectangle.y>=600){
+                rectangle.y+=10;
+                gone = true;
+                
+            }
+            else{
+                rectangle.y+=10;
+                Thread.sleep( 100 );
+            }
+
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
